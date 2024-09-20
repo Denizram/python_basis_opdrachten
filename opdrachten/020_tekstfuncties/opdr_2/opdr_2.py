b1906c97d212542eeed7717a6bd9d4b5e6537b2f
Denizram <97058756@st.deltion.nl>
2024-09-20 10:41:06 +0200

    20-9-24

--- opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
+++ opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
@@ -6,4 +6,10 @@
 
 my_list = ...
 
-print()
+my_string = "Tinus gaat op zijn tandem naar de hottentottententoonstelling" 
+aantal = my_string.count("t")
+eerste = "De letter \"t\" komt "
+tweede = "x voor in my_string"
+# message = f"{eerste}{aantal}{tweede}"
+#print(message)
+print(f"{eerste}{aantal}{tweede}")
