7e6e777b0e7871d4bcaeac5c0196afe1383d2eed
Denizram <97058756@st.deltion.nl>
2024-09-27 11:52:48 +0200

    27-9 11.52

--- opdrachten/030_berekeningen/opdr_1/opdr_1.py
+++ opdrachten/030_berekeningen/opdr_1/opdr_1.py
@@ -3,3 +3,14 @@
 # Groep:
 
 # Hier komt je code...
+
+deniz = 23
+dennis = 24
+iwan = 25
+
+totaal = deniz + dennis + iwan
+gemiddelde = totaal / 3
+
+print ("Dit weten we over joe, bob en fred: ")
+print ("De totale waarde van alle leeftijden bij elkaar opgeteld = ", totaal)
+print ("De gemiddelde leeftijd van deze boys is= ", gemiddelde)
--- opdrachten/030_berekeningen/opdr_2/opdr_2.py
+++ opdrachten/030_berekeningen/opdr_2/opdr_2.py
@@ -4,7 +4,23 @@
 
 # Hier komt je code...
 
-c = ...
-f = ...
+c1 = -12
+f1 = c1 * 9 / 5 + 32
+f1r = round(f1, 1)
 
-print()
+f2 = 102
+c2 = (f2 - 32) * 5 / 9
+c2r = round(c2, 1)
+
+c3 = 62.2
+f3 = c3 * 9 / 5 + 32
+f3r = round(f3, 1)
+
+f4 = 96
+c4 = (f4 - 32) * 5 / 9
+c4r = round(c4, 1)
+
+print (f1r)
+print (c2r)
+print (f3r)
+print (c4r)
