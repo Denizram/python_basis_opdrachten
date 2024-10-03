5143a252d325bc466cd46a996b8f7e15beccb73e
Denizram <97058756@st.deltion.nl>
2024-10-03 14:35:33 +0200

    3-10

--- opdrachten/040_lists/opdr_1/opdr_1.py
+++ opdrachten/040_lists/opdr_1/opdr_1.py
@@ -2,8 +2,16 @@
 # Naam student:
 # Groep:
 
-mylist = ...
-dict_1 = ...
-dict_2 = ...
-dict_3 = ...
-dict_4 = ...
+mylist = [1, 3, 7, 11]
+dict_1 = {"voornaam": "Deniz", "achternaam": "Ram", "Leeftijd": "23" }
+dict_2 = {"voornaam": "Dylan", "achternaam": "Bijkerk", "Leeftijd": "25" }
+dict_3 = {"voornaam": "Martijn", "achternaam": "Vincent", "Leeftijd": "22" }
+dict_4 = {"voornaam": "Lisette", "achternaam": "Vincent", "Leeftijd": "22" }
+gegevens = list(dict_1.keys())
+
+print (gegevens)
+print (mylist)
+print (dict_1)
+print (dict_2)
+print (dict_3)
+print (dict_4)
--- opdrachten/050_input_function/opdr_1/opdr_1.py
+++ opdrachten/050_input_function/opdr_1/opdr_1.py
@@ -4,4 +4,15 @@
 
 # Hier komt je code, maak gebruik van de input functie om de lengte van de rechthoekzijden van de driehoek op te vragen.
 
+import math
 
+try:
+    eerste = int(input("Geef lengte eerste zijde aan \n"))
+    tweede = int(input("Geef lengte tweede zijde aan \n"))
+
+    derde = eerste**2 + tweede**2
+    antwoord = math.sqrt(derde)
+
+    print (antwoord)
+except ValueError:
+   print("Hey dat is geen getal!")
